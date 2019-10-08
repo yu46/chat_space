6c0bf6f90fc30558e2749a283bc97a8199e9f6cc
yu46 <nasu.y@hotmail.co.jp>
2019-10-08 12:50:00 +0900

    create messageModel

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
-  has_many :massages #messageはいらない？状況で判断？
+  has_many :massages
   validates :users, presence: true
 end
